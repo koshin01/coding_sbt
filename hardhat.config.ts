45138450f8cfaccc773ed9c61b107790634e1f6f
koshin <ekoshin2001@icloud.com>
2023-08-16 17:17:34 +0900

    パブリックチェーン

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const getPrivateKey = (): string => {
+  if (typeof process.env.PRIVATE_KEY === "undefined") {
+    throw new TypeError("Add the private key to the environment variable");
+  } else {
+    return process.env.PRIVATE_KEY;
+  }
+}
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    mumbai: {
+      url: process.env.MUMBAI_NODE,
+      accounts: [getPrivateKey()],
+    }
+  },
 };
 
 export default config;
